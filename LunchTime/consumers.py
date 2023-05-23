2049fe649486ab1bd4500eafa986e4679e9d7a32
ChenQiqian <cqqqwq@outlook.com>
2023-05-24 00:35:41 +0800

    feat: add websocket server

---
+++ LunchTime/consumers.py
@@ -0,0 +1,56 @@
+import json
+
+import channels.layers
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.send_user_id = self.scope["url_route"]["kwargs"]["sender_id"]
+        self.receive_user_id = self.scope["url_route"]["kwargs"]["receiver_id"]
+        # generate a group name determined by the sender and receiver, discarding the order
+        self.room_group_name = (
+            f"chat_{min(self.send_user_id, self.receive_user_id)}_{max(self.send_user_id, self.receive_user_id)}"
+        )
+        print(self.send_user_id, self.receive_user_id)
+        await self.channel_layer.group_add(
+            self.room_group_name, self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name, self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+                "sender_id": self.send_user_id,
+                "receiver_id": self.receive_user_id,
+            },
+        )
+    
+    async def chat_message(self, event):
+        message = event["message"]
+        sender_id = event["sender_id"]
+        receiver_id = event["receiver_id"]
+
+        if receiver_id == self.send_user_id:
+            await self.send(
+                text_data=json.dumps(
+                    {
+                        "message": message,
+                        "sender_id": sender_id,
+                    }
+                )
+            )
+    
+    
