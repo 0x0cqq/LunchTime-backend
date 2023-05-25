cdc14a3f2485c79185d2abd9de040be98bc852d6
ChenQiqian <cqqqwq@outlook.com>
2023-05-25 14:00:59 +0800

    feat: chat history

--- LunchTime/consumers.py
+++ LunchTime/consumers.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 import json
 
 import channels.layers
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-
+from channels.db import database_sync_to_async
+from .models import User, ChatMessage
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -23,23 +25,70 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
+    
+    @database_sync_to_async
+    def insert_message(self, chat_message: ChatMessage):
+        chat_message.save()
+
+    @database_sync_to_async
+    def get_history(self, sender_id, receiver_id):
+        # get all the messages between the sender and receiver, both way
+        # the messages are ordered by create_time, most recent last
+        # return a list of messages
+        return ChatMessage.objects.filter(
+            sender_id__in=[sender_id, receiver_id],
+            receiver_id__in=[sender_id, receiver_id],
+        ).order_by("create_time")
+    
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        sender_id: int = text_data_json["sender_id"]
-        message: str = text_data_json["message"]
-        timestamp: int = text_data_json["timestamp"]
+        if text_data_json["type"] == "message":
+            message = text_data_json["message"]
+            content = message["message"]
+            # timestamp is in milliseconds
+            time = datetime.time()
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "chat_message",
-                "message": message,
-                "sender_id": self.send_user_id,
-                "receiver_id": self.receive_user_id,
-                "timestamp": timestamp,
-            },
-        )
+            # insert the message into the database
+
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "chat_message",
+                    "sender_id": self.send_user_id,
+                    "receiver_id": self.receive_user_id,
+                    "message": content,
+                    "timestamp": time.timestamp().__floor__(),
+                },
+            )
+            
+            await self.insert_message(ChatMessage(
+                sender_id=self.send_user_id,
+                receiver_id=self.receive_user_id,
+                message=content,
+                create_time=time,
+            ))
+        elif text_data_json["type"] == "history":
+            chat_list = await self.get_history(self.send_user_id, self.receive_user_id)
+            await self.send(
+                text_data=json.dumps(
+                    {
+                        "type": "history",
+                        "history": [
+                            {
+                                "sender_id": chat.sender_id,
+                                "message": chat.message,
+                                "timestamp": chat.create_time.timestamp().__floor__(),
+                            }
+                            for chat in chat_list
+                        ]
+                    }
+                )
+            )
+        else:
+            return
+
+
     
     async def chat_message(self, event):
         message = event["message"]
@@ -51,9 +100,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.send(
                 text_data=json.dumps(
                     {
-                        "message": message,
-                        "sender_id": sender_id,
-                        "timestamp": timestamp,
+                        "type": "message",
+                        "message": {
+                            "sender_id": sender_id,
+                            "message": message,
+                            "timestamp": timestamp,
+                        }
                     }
                 )
             )
