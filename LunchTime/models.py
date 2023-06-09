6c67435f66e1dcd2e357dd0ff009b854609aa053
SaltyFish-er <1742412922@qq.com>
2023-06-09 21:00:03 +0800

    fix: create_time bugs again

--- LunchTime/models.py
+++ LunchTime/models.py
@@ -26,7 +26,7 @@ class VerifyEmail(models.Model):
 class Post(models.Model):
     post_id = models.AutoField(primary_key=True)
     user_id = models.IntegerField()
-    create_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
     tag = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=1000)
@@ -47,7 +47,7 @@ class PostVideo(models.Model):
 class PostLove(models.Model):
     post_id = models.IntegerField()
     user_id = models.IntegerField()
-    create_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
     isRead = models.BooleanField(default=False)
     def __str__(self):
         return "postId: " + str(self.post_id) + "  userId: " + str(self.user_id)
@@ -55,14 +55,14 @@ class PostLove(models.Model):
 class PostSave(models.Model):
     post_id = models.IntegerField()
     user_id = models.IntegerField()
-    create_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return "postId: " + str(self.post_id) + "  userId: " + str(self.user_id)
 
 class PostComment(models.Model):
     post_id = models.IntegerField()
     user_id = models.IntegerField()
-    create_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
     comment = models.CharField(max_length=200)
     isRead = models.BooleanField(default=False)
 
